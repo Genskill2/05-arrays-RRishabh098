fe6cc4edb46cb7e817afdc545c20b5273d7d654d
Ram Rishabh Kumar <rishabhkmr098@gmail.com>
2021-06-19 23:59:22 +0530

    Update test_min.c

--- test_min.c
+++ test_min.c
@@ -2,7 +2,13 @@
 #include <assert.h>
 
 
-int min(int [], int);
+int min(int nums[], int size){
+  int min = 10000;
+  for(int i = 0; i < size; i++){
+    if(nums[i] < min) min = nums[i];
+  }
+  return min;
+}
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
